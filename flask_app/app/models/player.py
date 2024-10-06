bd6f43b51e0a91ca41ad9c505cb22f7045ab7dc0
Willem van Heemstra <wvanheemstra@icloud.com>
2024-10-06 17:13:43 +0200

    Update

---
+++ flask_app/app/models/player.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+from app.extensions import db
+
+class Player(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    character_id = db.Column(db.Integer, db.ForeignKey('character.id'), nullable=False)
+    score = db.Column(db.Integer, default=0)  # Example field to track player score
+    level = db.Column(db.Integer, default=1)  # Example field to track player level
+
+    user = db.relationship('User', backref='players')  # Relationship with User
+    character = db.relationship('Character', backref='players')  # Relationship with Character
+
+    def __repr__(self):
+        return f'<Player User ID: {self.user_id}, Character ID: {self.character_id}>'
