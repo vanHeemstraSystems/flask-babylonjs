2c19515afddae7ef42304c76191698c309b62c79
wvanheemstra <wvanheemstra@users.noreply.github.com>
2024-10-07 14:04:05 +0200

    Update game.py

--- flask_app/app/models/game.py
+++ flask_app/app/models/game.py
@@ -2,6 +2,7 @@
 from app.extensions import db
 
 class Game(db.Model):
+    __tablename__ = 'game'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     description = db.Column(db.Text, nullable=True)
