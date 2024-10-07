ebd6dbffe6beec4b4a5a413bcc1e30ef8a3df01f
wvanheemstra <wvanheemstra@users.noreply.github.com>
2024-10-07 13:12:53 +0200

    Update field.py

--- flask_app/app/models/field.py
+++ flask_app/app/models/field.py
@@ -7,7 +7,7 @@ class Field(db.Model):
     description = db.Column(db.Text, nullable=True)
     field_type_id = db.Column(db.Integer, db.ForeignKey('field_type.id'), nullable=False)  # Foreign Key to FieldType
     board_id = db.Column(db.Integer, db.ForeignKey('board.id'), nullable=False)  # Foreign Key to Board
-
+    created_at = db.Column(db.DateTime, nullable=False, default=db.func.current_timestamp())
     field_type = db.relationship('FieldType', backref='fields')  # Many-to-one relationship
     board = db.relationship('Board', backref='fields')  # One-to-many relationship
 
