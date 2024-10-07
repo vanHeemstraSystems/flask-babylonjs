65b43800336e3bb795cff94be26fbebc6f209168
wvanheemstra <wvanheemstra@users.noreply.github.com>
2024-10-07 14:07:41 +0200

    Update tag.py

--- flask_app/app/models/tag.py
+++ flask_app/app/models/tag.py
@@ -2,6 +2,7 @@
 from app.extensions import db
 
 class Tag(db.Model):
+   __tablename__ = 'tag'
    id = db.Column(db.Integer, primary_key=True)
    title = db.Column(db.String(100), nullable=False, unique=True)
 
