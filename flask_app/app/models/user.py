34cfd1246211125a0968c294f36fd14755926e2a
wvanheemstra <wvanheemstra@users.noreply.github.com>
2024-10-07 11:24:45 +0200

    Update user.py

--- flask_app/app/models/user.py
+++ flask_app/app/models/user.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python
-from app.extensions import db
+from app.extensions import db, bcrypt
 
-class Scene(db.Model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    scene_number = db.Column(db.String(50), nullable=True)
-    characters = db.relationship('Character', secondary='character_scene', backref='scenes', lazy='dynamic')
-    props = db.relationship('Prop', secondary='prop_scene', backref='scenes', lazy='dynamic')
-    lights = db.relationship('Light', secondary='light_scene', backref='scenes', lazy='dynamic')
-    tags = db.relationship('Tag', secondary='scene_tag', backref='scenes', lazy='dynamic')
-    shots = db.relationship('Shot', backref='scene', lazy='dynamic')  # One-to-many relationship with Shot
+    username = db.Column(db.String(100), nullable=False, unique=True)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+    password_hash = db.Column(db.String(128), nullable=False)
 
     def __repr__(self):
-        return f'<Scene {self.title}, Number: {self.scene_number}>'
+        return f'<User {self.username}>'
+
+    def set_password(self, password):
+        self.password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
+
+    def check_password(self, password):
+        return bcrypt.check_password_hash(self.password_hash, password)
