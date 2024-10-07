65b0c51d44312322eb1f7b84042cdf89faef6a45
wvanheemstra <wvanheemstra@users.noreply.github.com>
2024-10-07 13:43:05 +0200

    Update light_type.py

--- flask_app/app/models/light_type.py
+++ flask_app/app/models/light_type.py
@@ -1,1 +1,11 @@
+#!/usr/bin/env python
+from app.extensions import db
 
+class LightType(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False, unique=True)
+    description = db.Column(db.Text, nullable=True)
+    created_at = db.Column(db.DateTime, nullable=False, default=db.func.current_timestamp())
+
+    def __repr__(self):
+        return f'<LightType {self.name}>'
