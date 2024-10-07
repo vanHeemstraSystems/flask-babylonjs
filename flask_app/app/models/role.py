ccdf41c2e337369d97068827cd0613332dea5219
wvanheemstra <wvanheemstra@users.noreply.github.com>
2024-10-07 10:57:39 +0200

    Update role.py

--- flask_app/app/models/role.py
+++ flask_app/app/models/role.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python
 from app.extensions import db
 
-# MORE
+class Role(db.Model):
+   id = db.Column(db.Integer, primary_key=True)
+   title = db.Column(db.String(50), nullable=False, unique=True)
+
+   def __repr__(self):
+       return f'<Role {self.title}>'
