005d702aa34f4d7321c0b8c5f9d1b10e9ca27c4d
wvanheemstra <wvanheemstra@users.noreply.github.com>
2024-10-07 13:10:09 +0200

    Update camera_type.py

--- flask_app/app/models/camera_type.py
+++ flask_app/app/models/camera_type.py
@@ -4,6 +4,7 @@ from app.extensions import db
 class CameraType(db.Model):
    id = db.Column(db.Integer, primary_key=True)
    title = db.Column(db.String(50), nullable=False, unique=True)
-
+   created_at = db.Column(db.DateTime, nullable=False, default=db.func.current_timestamp())
+   
    def __repr__(self):
        return f'<CameraType {self.title}>'
