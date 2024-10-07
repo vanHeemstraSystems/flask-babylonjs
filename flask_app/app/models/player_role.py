492025e130d1363184d8f660cbfab2157b118aa5
wvanheemstra <wvanheemstra@users.noreply.github.com>
2024-10-07 12:05:43 +0200

    Update player_role.py

--- flask_app/app/models/player_role.py
+++ flask_app/app/models/player_role.py
@@ -2,9 +2,12 @@
 from app.extensions import db
 
 class PlayerRole(db.Model):
+    __tablename__ = 'player_role'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False, unique=True)
     description = db.Column(db.String(200), nullable=True)
+    player_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    player = db.relationship('Player', backref='user_roles') # Relationship with Player
 
     def __repr__(self):
-        return f'<PlayerRole {self.name}>'
+        return f'<PlayerRole Player ID: {self.player_id}>'
