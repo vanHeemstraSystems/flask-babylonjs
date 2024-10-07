45a506560f70e461f2dc6d5c4dd5d6cbc11533e1
wvanheemstra <wvanheemstra@users.noreply.github.com>
2024-10-07 13:11:25 +0200

    Update character_role.py

--- flask_app/app/models/character_role.py
+++ flask_app/app/models/character_role.py
@@ -8,6 +8,7 @@ class CharacterRole(db.Model):
     description = db.Column(db.String(200), nullable=True)
     character_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     character = db.relationship('Character', backref='character_roles') # Relationship with Character
+    created_at = db.Column(db.DateTime, nullable=False, default=db.func.current_timestamp())
 
     def __repr__(self):
         return f'<CharacterRole Character ID: {self.character_id}>'
