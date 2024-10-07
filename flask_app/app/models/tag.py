7cf4d2faf220114ff7dfd7d5dd6b19fc9074105c
wvanheemstra <wvanheemstra@users.noreply.github.com>
2024-10-07 14:09:12 +0200

    Update tag.py

--- flask_app/app/models/tag.py
+++ flask_app/app/models/tag.py
@@ -5,6 +5,7 @@ class Tag(db.Model):
    __tablename__ = 'tag'
    id = db.Column(db.Integer, primary_key=True)
    title = db.Column(db.String(100), nullable=False, unique=True)
+   created_at = db.Column(db.DateTime, nullable=False, default=db.func.current_timestamp())
 
    def __repr__(self):
        return f'<Tag {self.title}>'
