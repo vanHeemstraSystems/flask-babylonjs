6da34b896c86a17543c34f0f1e55f53c2adc5b0b
wvanheemstra <wvanheemstra@users.noreply.github.com>
2024-10-07 15:01:02 +0200

    Update story.py

--- flask_app/app/models/story.py
+++ flask_app/app/models/story.py
@@ -1,1 +1,15 @@
+#!/usr/bin/env python
+from app.extensions import db
 
+class Story(db.Model):
+    __tablename__ = 'story'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    release_date = db.Column(db.Date, nullable=True)
+    genre = db.Column(db.String(50), nullable=True)
+    content = db.Column(db.Text, nullable=True)
+    created_at = db.Column(db.DateTime, nullable=False, default=db.func.current_timestamp())
+
+    def __repr__(self):
+        return f'<Story {self.title}>'
