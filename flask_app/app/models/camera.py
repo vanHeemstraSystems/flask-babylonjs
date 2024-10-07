9b419522f70a52e569e116aa6bf0b52dbdc20d1a
wvanheemstra <wvanheemstra@users.noreply.github.com>
2024-10-07 13:09:53 +0200

    Update camera.py

--- flask_app/app/models/camera.py
+++ flask_app/app/models/camera.py
@@ -5,7 +5,7 @@ class Camera(db.Model):
    id = db.Column(db.Integer, primary_key=True)
    camera_type_id = db.Column(db.Integer, db.ForeignKey('camera_type.id'), nullable=False)
    settings = db.Column(db.String(200), nullable=False)
-
+   created_at = db.Column(db.DateTime, nullable=False, default=db.func.current_timestamp())
    camera_type = db.relationship('CameraType', backref='cameras')
 
    def __repr__(self):
