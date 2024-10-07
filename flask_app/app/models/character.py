9597ec621bed7d62a84ce0296bc564fffc412755
wvanheemstra <wvanheemstra@users.noreply.github.com>
2024-10-07 13:10:26 +0200

    Update character.py

--- flask_app/app/models/character.py
+++ flask_app/app/models/character.py
@@ -5,6 +5,7 @@ class Character(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     model_url = db.Column(db.String(200), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=db.func.current_timestamp())
 
     def __repr__(self):
         return f'<Character {self.name}>'
