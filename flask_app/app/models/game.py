db5d4a1a7413650c5dbbb03f5657651133b499c6
wvanheemstra <wvanheemstra@users.noreply.github.com>
2024-10-07 13:39:16 +0200

    Update game.py

--- flask_app/app/models/game.py
+++ flask_app/app/models/game.py
@@ -7,8 +7,9 @@ class Game(db.Model):
     description = db.Column(db.Text, nullable=True)
     release_date = db.Column(db.Date, nullable=True)
     genre = db.Column(db.String(50), nullable=True)
+    board_id = db.Column(db.Integer, db.ForeignKey('board.id'), nullable=False) # Foreign Key to Board
     created_at = db.Column(db.DateTime, nullable=False, default=db.func.current_timestamp())
-
+    board = db.relationship('Board', backref='game', uselist=False) # One-to-one relationship
     users = db.relationship('User', secondary='user_game', backref='games')
 
     def __repr__(self):
