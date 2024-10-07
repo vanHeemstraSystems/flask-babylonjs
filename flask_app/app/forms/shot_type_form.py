816d3a90959c356f076e2431c6a5cdba4bd21ecc
wvanheemstra <wvanheemstra@users.noreply.github.com>
2024-10-07 16:33:11 +0200

    Create shot_type_form.py

---
+++ flask_app/app/forms/shot_type_form.py
@@ -0,0 +1,8 @@
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, SubmitField from wtforms.validators import DataRequired
+
+class ShotTypeForm(FlaskForm):
+    name = StringField('Shot Type Name', validators=[DataRequired()])
+    description = TextAreaField('Description')
+    submit = SubmitField('Submit')
