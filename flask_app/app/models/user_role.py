f320ae189bdc32f5760b88768faa7ecd2859f72a
wvanheemstra <wvanheemstra@users.noreply.github.com>
2024-10-07 14:09:38 +0200

    Update user_role.py

--- flask_app/app/models/user_role.py
+++ flask_app/app/models/user_role.py
@@ -7,6 +7,7 @@ class UserRole(db.Model):
     name = db.Column(db.String(50), nullable=False, unique=True)
     description = db.Column(db.String(200), nullable=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=db.func.current_timestamp())
     user = db.relationship('User', backref='user_roles') # Relationship with User
 
     def __repr__(self):
