b9278edac42513e16e01bd5dfaf8e8e60a2e7d06
wvanheemstra <wvanheemstra@users.noreply.github.com>
2024-10-07 16:26:55 +0200

    Update camera.py

--- flask_app/app/models/camera.py
+++ flask_app/app/models/camera.py
@@ -4,7 +4,7 @@ from app.extensions import db
 class Camera(db.Model):
    __tablename__ = 'camera'
    id = db.Column(db.Integer, primary_key=True)
-   camera_type_id = db.Column(db.Integer, db.ForeignKey('camera_type.id'), nullable=False)
+   camera_type_id = db.Column(db.Integer, db.ForeignKey('camera_type.id'), nullable=False) # Foreign Key to CameraType
    settings = db.Column(db.String(200), nullable=False)
    created_at = db.Column(db.DateTime, nullable=False, default=db.func.current_timestamp())
    camera_type = db.relationship('CameraType', backref='cameras') # Many-to-one relationship with CameraType
