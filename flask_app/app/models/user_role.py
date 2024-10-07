cc05399dfd8fb59ae293f848d4b27fe0bd78d1d5
wvanheemstra <wvanheemstra@users.noreply.github.com>
2024-10-07 12:04:17 +0200

    Update user_role.py

--- flask_app/app/models/user_role.py
+++ flask_app/app/models/user_role.py
@@ -2,9 +2,12 @@
 from app.extensions import db
 
 class UserRole(db.Model):
+    __tablename__ = 'user_role'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False, unique=True)
     description = db.Column(db.String(200), nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user = db.relationship('User', backref='user_roles') # Relationship with User
 
     def __repr__(self):
-        return f'<UserRole {self.name}>'
+        return f'<UserRole User ID: {self.user_id}>'
