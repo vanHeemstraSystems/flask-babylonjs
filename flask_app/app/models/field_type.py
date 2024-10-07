6ef453c7d6befa814edf2f131bc714ae5c7c0cb0
wvanheemstra <wvanheemstra@users.noreply.github.com>
2024-10-07 13:27:14 +0200

    Update field_type.py

--- flask_app/app/models/field_type.py
+++ flask_app/app/models/field_type.py
@@ -5,6 +5,7 @@ class FieldType(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False, unique=True)
     description = db.Column(db.Text, nullable=True)
+    created_at = db.Column(db.DateTime, nullable=False, default=db.func.current_timestamp())
 
     def __repr__(self):
         return f'<FieldType {self.name}>'
