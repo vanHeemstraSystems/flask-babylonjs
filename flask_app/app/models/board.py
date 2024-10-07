2683391d0a88c48a3a590bbe2e9d38a9501d381b
wvanheemstra <wvanheemstra@users.noreply.github.com>
2024-10-07 14:06:11 +0200

    Update board.py

--- flask_app/app/models/board.py
+++ flask_app/app/models/board.py
@@ -2,6 +2,7 @@
 from app.extensions import db
 
 class Board(db.Model):
+    __tablename__ = 'board'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     description = db.Column(db.Text, nullable=True)
