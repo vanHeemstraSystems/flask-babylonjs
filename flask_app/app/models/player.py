cac0d1c0584e37a786959f55a2c462e04e0925ac
wvanheemstra <wvanheemstra@users.noreply.github.com>
2024-10-07 13:51:07 +0200

    Update player.py

--- flask_app/app/models/player.py
+++ flask_app/app/models/player.py
@@ -7,6 +7,7 @@ class Player(db.Model):
     character_id = db.Column(db.Integer, db.ForeignKey('character.id'), nullable=False)
     score = db.Column(db.Integer, default=0)  # Example field to track player score
     level = db.Column(db.Integer, default=1)  # Example field to track player level
+    created_at = db.Column(db.DateTime, nullable=False, default=db.func.current_timestamp())
 
     user = db.relationship('User', backref='players')  # Relationship with User
     character = db.relationship('Character', backref='players')  # Relationship with Character
