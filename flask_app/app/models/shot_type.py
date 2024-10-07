62fb16eba2c295d53dce953ad0b1fd1509c2ad0f
wvanheemstra <wvanheemstra@users.noreply.github.com>
2024-10-07 15:34:58 +0200

    Update shot_type.py

--- flask_app/app/models/shot_type.py
+++ flask_app/app/models/shot_type.py
@@ -1,1 +1,11 @@
+#!/usr/bin/env python
+from app.extensions import db
 
+class ShotType(db.Model):
+   __tablename__ = 'shot_type'
+   id = db.Column(db.Integer, primary_key=True)
+   title = db.Column(db.String(50), nullable=False, unique=True)
+   created_at = db.Column(db.DateTime, nullable=False, default=db.func.current_timestamp())
+   
+   def __repr__(self):
+       return f'<ShotType {self.title}>'
