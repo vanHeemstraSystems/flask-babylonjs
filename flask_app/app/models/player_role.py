b7105c15581d56a868c8cc2a28cb933283f2c190
wvanheemstra <wvanheemstra@users.noreply.github.com>
2024-10-07 14:02:14 +0200

    Update player_role.py

--- flask_app/app/models/player_role.py
+++ flask_app/app/models/player_role.py
@@ -7,6 +7,7 @@ class PlayerRole(db.Model):
     name = db.Column(db.String(50), nullable=False, unique=True)
     description = db.Column(db.String(200), nullable=True)
     player_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=db.func.current_timestamp())
     player = db.relationship('Player', backref='player_roles') # Relationship with Player
 
     def __repr__(self):
