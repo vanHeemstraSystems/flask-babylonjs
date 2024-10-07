e99efb7dbd7efc8bc2f3d215d23dff8f5434ab9e
wvanheemstra <wvanheemstra@users.noreply.github.com>
2024-10-07 14:06:35 +0200

    Update prop.py

--- flask_app/app/models/prop.py
+++ flask_app/app/models/prop.py
@@ -2,6 +2,7 @@
 from app.extensions import db
 
 class Prop(db.Model):
+    __tablename__ = 'prop'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     model_url = db.Column(db.String(200), nullable=False)
