47e64a723dab099b868d4a2e70b2de93fb352c40
wvanheemstra <wvanheemstra@users.noreply.github.com>
2024-10-07 15:37:59 +0200

    Update shot.py

--- flask_app/app/models/shot.py
+++ flask_app/app/models/shot.py
@@ -8,9 +8,10 @@ class Shot(db.Model):
     description = db.Column(db.Text, nullable=True)
     camera_settings = db.Column(db.String(200), nullable=False)  # JSON settings for the camera
     scene_id = db.Column(db.Integer, db.ForeignKey('scene.id'), nullable=False)  # Foreign Key to Scene
+    shot_type_id = db.Column(db.Integer, db.ForeignKey('shot_type.id'), nullable=False) # Foreign Key to ShotType
     created_at = db.Column(db.DateTime, nullable=False, default=db.func.current_timestamp())
-
     scene = db.relationship('Scene', backref='shots')  # One-to-many relationship with Scene
+    shot_type = db.relationship('ShotType', backref='shots') # One-to-many relationship with ShotType
 
     def __repr__(self):
         return f'<Shot {self.title}>'
