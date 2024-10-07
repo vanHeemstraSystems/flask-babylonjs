0be0d5c1a49d5b03729b10d9974ca22c5faf14f2
wvanheemstra <wvanheemstra@users.noreply.github.com>
2024-10-07 13:40:37 +0200

    Update light.py

--- flask_app/app/models/light.py
+++ flask_app/app/models/light.py
@@ -7,6 +7,7 @@ class Light(db.Model):
     light_type = db.Column(db.String(50), nullable=False)
     intensity = db.Column(db.Float, default=1.0)
     position = db.Column(db.String(100), nullable=True)
+    created_at = db.Column(db.DateTime, nullable=False, default=db.func.current_timestamp())
 
     def __repr__(self):
         return f'<Light {self.name}, Light_Type: {self.light_type}, Intensity: {self.intensity}>'
