463dedc5f3b60c1d4b0df65308f559ecf48a8167
wvanheemstra <wvanheemstra@users.noreply.github.com>
2024-10-07 11:53:56 +0200

    Update character_role.py

--- flask_app/app/models/character_role.py
+++ flask_app/app/models/character_role.py
@@ -1,1 +1,10 @@
+#!/usr/bin/env python
+from app.extensions import db
 
+class CharacterRole(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False, unique=True)
+    description = db.Column(db.String(200), nullable=True)
+
+    def __repr__(self):
+        return f'<CharacterRole {self.name}>'
