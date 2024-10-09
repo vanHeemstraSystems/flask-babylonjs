6b7ed5d385347dd721f0e91699c25811c85a3feb
wvanheemstra <wvanheemstra@users.noreply.github.com>
2024-10-09 08:50:14 +0200

    Update __init__.py

--- flask_app/app/__init__.py
+++ flask_app/app/__init__.py
@@ -29,9 +29,8 @@ def create_app(config_class=Config):
     # from app.scene3 import bp as scene3_bp
     # app.register_blueprint(scene3_bp, url_prefix='/scene3')
 
-    from app.routes.board_routes import boards_bp
-    # from app.boards import bp as boards_bp
-    app.register_blueprint(boards_bp, url_prefix='/boards')
+    from app.routes.board_routes import board_bp
+    app.register_blueprint(board_bp, url_prefix='/boards')
 
     from app.cameras import bp as cameras_bp
     app.register_blueprint(cameras_bp, url_prefix='/cameras')
