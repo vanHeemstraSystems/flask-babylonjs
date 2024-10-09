6c07de8893721874f9b27c79ca600d3f14957f37
wvanheemstra <wvanheemstra@users.noreply.github.com>
2024-10-09 08:58:52 +0200

    Update

--- flask_app/app/routes/camera_routes.py
+++ flask_app/app/routes/camera_routes.py
@@ -3,7 +3,7 @@ from flask import Blueprint, render_template, request, redirect, url_for, flash
 from app.models.camera import Camera
 from app.models.camera_type import CameraType
 from app.forms.camera_form import CameraForm
-from app import db
+from app.extensions import db
 
 camera_bp = Blueprint('camera', __name__)
 
--- flask_app/app/routes/main_routes.py
+++ flask_app/app/routes/main_routes.py
@@ -1,1 +1,8 @@
+#!/usr/bin/env python
+from flask import Blueprint, render_template, request, redirect, url_for, flash
 
+main_bp = Blueprint('main', __name__)
+
+@bp.route('/')
+def index():
+    return render_template('index.html')
