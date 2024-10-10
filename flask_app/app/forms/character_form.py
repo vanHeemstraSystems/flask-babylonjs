ca5e673c5c36a7511f17254f29df67027adb7ee4
Willem van Heemstra <wvanheemstra@icloud.com>
2024-10-10 14:56:00 +0200

    Update

--- flask_app/app/forms/character_form.py
+++ flask_app/app/forms/character_form.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 from flask_wtf import FlaskForm
-from wtforms import StringField, SelectField, FloatField, SubmitField
+from wtforms import StringField, TextAreaField, SubmitField
 from wtforms.validators import DataRequired
 
 class CharacterForm(FlaskForm):
