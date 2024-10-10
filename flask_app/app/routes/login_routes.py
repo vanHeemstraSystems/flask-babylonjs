f50870a15e236bd15521c3f387abdfc3058b6c54
Willem van Heemstra <wvanheemstra@icloud.com>
2024-10-10 11:09:18 +0200

    Update

--- flask_app/app/routes/login_routes.py
+++ flask_app/app/routes/login_routes.py
@@ -2,9 +2,22 @@
 from app.extensions import db, bcrypt
 from app.decorators.login_decorators import login_forbidden
 from app.forms.login_form import LoginForm
-#from app.forms.registration_form import RegistrationForm
 from app.models.user import User
 from flask import Blueprint, redirect, url_for, render_template, flash, request
 from flask_login import login_user, logout_user, login_required
 
 login = Blueprint('login', __name__)
+
+@login.route('/login', methods=['GET', 'POST'])
+@login_forbidden
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            flash('logged in')
+            return redirect(next_page) if next_page else redirect(url_for('main.index'))
+        flash('error', 'danger')
+    return render_template('login/login.html', form=form)
